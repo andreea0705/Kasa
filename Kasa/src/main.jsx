548b024db3c90ee94a1ffea15516968ae949d226
Andreea Taflan <andreea07.05.2001@gmail.com>
2025-06-22 18:50:37 +0200

    resolving some import files

--- Kasa/src/main.jsx
+++ Kasa/src/main.jsx
@@ -1,8 +1,7 @@
 // Point de demarage, il affiche <App /> dans #root
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'; // ➕ Ajout pour le routage
-import './index.css'
+import './css/index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
